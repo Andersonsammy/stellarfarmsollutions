fe918f85fb274bd1bda06b7b2fd6bf28b6bfdbf2
Andersonsammy <andersonsammy40@gmail.com>
2026-04-17 22:46:05 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -84,11 +84,7 @@ export default function Home() {
         <div className="max-w-[1200px] mx-auto px-6 md:px-10">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
             <Reveal w-full>
-              <PremiumImage 
-                  src="/images/siol_testing.jpg.jpeg" 
-                  alt="Rich soil and farming landscape in Kisii"
-                  className="w-full h-[600px] shadow-2xl rounded-[2.5rem]"
-              />
+             public/images/.gitkeep
             </Reveal>
             
             <Reveal delay={0.2}>
