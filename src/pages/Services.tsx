e4943ab7cd4c46a1351d3ad431453db35102cf25
Andersonsammy <andersonsammy40@gmail.com>
2026-04-17 17:48:05 +0300

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -22,12 +22,12 @@ export default function Services() {
             </Reveal>
           </div>
           
-          <Reveal delay={0.2} className="w-full">
-             <img 
-  src="https://images.unsplash.com/photo-1625246333195-78d9c38ad449?w=1200&auto=format&fit=crop"
-  alt="Agri expert inspecting crops"
-  className="w-full h-[40vh] md:h-[60vh] rounded-[2rem] shadow-2xl object-cover"
-/>
+         <Reveal delay={0.2} className="w-full">
+             <PremiumImage 
+                src="https://i.pinimg.com/originals/89/ad/f6/89adf6be9b976101c1de7df4c315c2a6.jpg" 
+                alt="Agri expert inspecting crops" 
+                className="w-full h-[40vh] md:h-[60vh] rounded-[2rem] shadow-2xl relative border border-black/5" 
+             />
           </Reveal>
         </div>
       </section>
